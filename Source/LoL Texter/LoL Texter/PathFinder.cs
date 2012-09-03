3b63035c122f6445056e32e50af17fa95925d147
Benajmin Kristensen <benjamin_kristensen@live.dk>
2012-09-03 09:59:06 +0200

    Comitting Initial solution

---
+++ Source/LoL Texter/LoL Texter/PathFinder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Win32;
+
+namespace LoL_Text_modder
+{
+	public static class PathFinder
+	{
+		public static string RADSpath;
+		public static string Locale;
+
+		static PathFinder()
+		{
+			var openSubKey = Registry.CurrentUser.OpenSubKey(@"Software\Riot Games\RADS");
+			if (openSubKey != null)
+				RADSpath = openSubKey.GetValue("LocalRootFolder").ToString();
+		}
+
+		public static string GetLocale()
+		{
+			string[] lines = File.ReadAllLines(RADSpath + @"\system\locale.cfg");
+			string[] lineParts = lines[0].Split('=');
+			Locale = lineParts[1].Trim();
+
+			return Locale;
+		}
+
+		public static string GetLatestFilePath(string locale)
+		{
+			return "";
+		}
+	}
+}
