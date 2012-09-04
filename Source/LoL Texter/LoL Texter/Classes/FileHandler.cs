28ab8933ee748c4d80b4f9b008203a83f0ee17ff
Benajmin Kristensen <benjamin_kristensen@live.dk>
2012-09-04 10:27:30 +0200

    Error checking and restore method

--- Source/LoL Texter/LoL Texter/Classes/FileHandler.cs
+++ Source/LoL Texter/LoL Texter/Classes/FileHandler.cs
@@ -11,34 +11,42 @@ namespace LoL_Texter.Classes
 	{
 		private string _defaultPath;
 		private string _defaultLocale;
+		private string _appData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
 
 		public FileHandler(string defaultPath)
 		{
 			_defaultPath = defaultPath;
 		}
 
-		public FileHandler(string defaultPath, string defaultLocale)
+		public FileHandler(string defaultPath, string defaultLocale) : this(defaultPath)
 		{
-			_defaultPath = defaultPath;
 			_defaultLocale = defaultLocale;
 		}
 
 		public bool DoBackup()
 		{
-			return DoBackup(_defaultPath, _defaultLocale);
+			if (_defaultLocale != null)
+			{
+				return DoBackup(_defaultPath, _defaultLocale);
+			}
+			return false;
 		}
 
 		public bool DoBackup(string fullPath)
 		{
 			string filename = Path.GetFileName(fullPath);
-			string locale = filename.Replace("fontconfig_", "");
-			return DoBackup(fullPath.Replace(filename, ""), locale.Replace(".txt", ""));
+			if (filename != null)
+			{
+				string locale = filename.Replace("fontconfig_", "");
+				return DoBackup(fullPath.Replace(filename, ""), locale.Replace(".txt", ""));
+			}
 		}
 
 		public bool DoBackup(string rootPath, string locale)
 		{
-			File.Copy(rootPath + @"\fontconfig_" + locale + ".txt", rootPath + @"\backup.loltxt");
-			if(File.Exists(_defaultPath+@"\backup.loltxt"))
+
+			File.Copy(Path.Combine(rootPath, "fontconfig_" + locale + ".txt"), Path.Combine(rootPath, "backup.LTX"));
+			if(File.Exists(Path.Combine(_defaultPath, "backup.loltxt")))
 			{
 				return true;
 			}
@@ -50,13 +58,42 @@ namespace LoL_Texter.Classes
 
 		public bool SaveLines(string [] lines)
 		{
-			if (!File.Exists(_defaultPath + @"\backup.loltxt"))
+			if(_defaultLocale != null)
+			{
+				return SaveLines(lines, _defaultLocale);
+			}
+			return false;
+		}
+
+		public bool SaveLines(string[] lines, string locale)
+		{
+			if (!File.Exists(Path.Combine(_defaultPath, "backup.loltxt")))
 			{
 				DoBackup();
 			}
 
-			File.WriteAllLines(_defaultPath + @"\fontconfig_" + _defaultLocale + ".txt", lines);
-			return File.ReadAllLines(_defaultPath + @"\fontconfig_" + _defaultLocale + ".txt").Equals(lines);
+			File.WriteAllLines(Path.Combine(_defaultPath, "fontconfig_" + locale + ".txt"), lines);
+			return File.ReadAllLines(Path.Combine(_defaultPath, "fontconfig_" + locale + ".txt")).Equals(lines);
+		}
+
+		public bool Restore()
+		{
+			if (_defaultLocale != null)
+			{
+				return Restore(_defaultLocale);
+			}
+			return false;
+		}
+
+		public bool Restore(string locale)
+		{
+			return Restore(_defaultPath, locale);
+		}
+
+		public bool Restore(string path, string locale)
+		{
+			File.Copy(Path.Combine(path, "backup.loltxt"), Path.Combine(path, "fontconfig_" + locale + ".txt"), true);
+			return File.ReadAllLines(Path.Combine(path, "fontconfig_" + locale + ".txt")).Equals(File.ReadAllLines(Path.Combine(path, "backup.loltxt")));
 		}
 	}
 }
