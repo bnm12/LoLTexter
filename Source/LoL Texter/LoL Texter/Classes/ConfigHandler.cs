f58cc5717c40fd5f0dc744b8c05df9d60e408948
Benajmin Kristensen <benjamin_kristensen@live.dk>
2012-09-28 17:58:29 +0200

    New class

---
+++ Source/LoL Texter/LoL Texter/Classes/ConfigHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoL_Texter.Classes
+{
+	class ConfigHandler
+	{
+		public Dictionary<string, string> Config;
+		private string _configFilePath;
+
+		public ConfigHandler(string configFilePath)
+		{
+			_configFilePath = configFilePath;
+		}
+	}
+}
