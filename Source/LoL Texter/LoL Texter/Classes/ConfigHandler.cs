1f9c38768ed1bfacafbbacb43c66133904526143
Benajmin Kristensen <benjamin_kristensen@live.dk>
2012-10-01 09:27:03 +0200

    Adding more methods to the class

--- Source/LoL Texter/LoL Texter/Classes/ConfigHandler.cs
+++ Source/LoL Texter/LoL Texter/Classes/ConfigHandler.cs
@@ -9,12 +9,57 @@ namespace LoL_Texter.Classes
 {
 	class ConfigHandler
 	{
-		public Dictionary<string, string> Config;
+		public Dictionary<string, string> Config = new Dictionary<string, string>();
+		private List<string> _headder = new List<string>();
 		private string _configFilePath;
 
 		public ConfigHandler(string configFilePath)
 		{
 			_configFilePath = configFilePath;
+			Config = readConfig(_configFilePath);
+		}
+
+		private Dictionary<string, string> readConfig(string path)
+		{
+			Dictionary<string, string> returnLines = new Dictionary<string, string>();
+			FileStream fs = new FileStream(path, FileMode.Open);
+			StreamReader reader = new StreamReader(fs);
+
+			while (!reader.EndOfStream)
+			{
+				string line = reader.ReadLine();
+				
+				if(line.StartsWith("tr "))
+				{
+					int firstIndex = line.IndexOf('"');
+					int secondIndex = line.IndexOf('"', firstIndex + 1);
+					string key = line.Substring(firstIndex + 1, secondIndex - (firstIndex + 1));
+
+					int thirdIndex = line.IndexOf('"', secondIndex + 1);
+					int fourthindex = line.IndexOf('"', thirdIndex + 1);
+					string value = line.Substring(thirdIndex + 1, fourthindex - (thirdIndex + 1));
+
+					returnLines.Add(key, value);
+				}
+				else
+				{
+					_headder.Add(line);
+				}
+			}
+			return returnLines;
+		}
+
+		public List<string> AssembleConfig()
+		{
+			List<string> returnList = new List<string>();
+			returnList.AddRange(_headder);
+
+			foreach (KeyValuePair<string, string> keyValuePair in Config)
+			{
+				returnList.Add("tr \"" + keyValuePair.Key + "\" = \"" + keyValuePair.Value + "\"");
+			}
+
+			return returnList;
 		}
 	}
 }
